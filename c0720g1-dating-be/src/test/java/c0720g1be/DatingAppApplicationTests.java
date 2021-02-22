55f3e31fee4121a862c45aa9fb9834e6ed0353b1
winback1000 <68835331+winback1000@users.noreply.github.com>
2021-02-23 02:33:00 +0700

    Khoa update base spring project

---
+++ c0720g1-dating-be/src/test/java/c0720g1be/DatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package c0720g1be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
